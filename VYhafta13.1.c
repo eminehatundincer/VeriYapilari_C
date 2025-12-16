7ddbf1c02fa9bbbc6d8df7322f60cd9aa15847a6
Emine Hatun Dinçer <98483384+eminehatundincer@users.noreply.github.com>
2025-12-16 12:23:26 +0300

    Update VYhafta13.1.c

--- VYhafta13.1.c
+++ VYhafta13.1.c
@@ -1,17 +1,29 @@
-#include<stdlib.h> 
-#include<stdio.h> 
-void bubbleSort(int* arr, int n) { 
-  int i, j, tmp; 
-  for (i = 0; i < n - 1; i++) {
-    for (j = i + 1;j < n;j++) { 
-  if (arr[i] < arr[j]) { tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp; 
-  } 
-  } 
-  } 
-} 
-int main() { 
-  int dizi[5] = { 1,10,3,4,8 }; 
-  bubbleSort(dizi, 5);
-  printf("%d\n", dizi[1]);
-  return 0; 
+#include<stdlib.h>
+#include<stdio.h>
+
+void bubbleSort(int* arr, int n) {
+	int i, j, tmp;
+	for (i = 0; i < n - 1; i++) {
+		for (j = i + 1;j < n;j++) {
+			if (arr[i] < arr[j]) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
 }
+int main() {
+	int dizi[5] = { 1,10,3,4,8 };
+	int i;
+
+	bubbleSort(dizi, 5);
+
+	// Tüm diziyi yazdır
+	for (i = 0; i < 5; i++) {
+		printf("%d ", dizi[i]);
+	}
+	return 0;
+}
+
+🟢 
